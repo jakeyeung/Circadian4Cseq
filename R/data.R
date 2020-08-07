ff4624ccfc84e0aac788118773d4d85ec7750974
Jake Yeung <jake.yeung@epfl.ch>
2020-08-07 15:35:41 +0200

    Add data for the github repo

---
+++ R/data.R
@@ -0,0 +1,106 @@
+#' 4Cseq objs
+#'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"counts.long.delt.lk.LR"
+
+#' 4Cseq objs
+#'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+
+#' 4Cseq objs
+#'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"bait_locations_batches_1-2"
+
+#' 4Cseq objs
+#'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Bmal1_TSS.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Cry1.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Cry1_TSS.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Cry1_UP.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Gys2_Exon8.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Gys2_TSS.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Hoxd4.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Per1_TSS.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"Per2_TSS.normmax.Inf.log.FALSE.nfrags.5.remove.auto.sig.2500.Robj"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+#'   ...
+#' }
+"counts.long.delt.lk.LR.batch_1_2"
+
+#' 4Cseq objs #'
+#' @format Objects for 4Cseq
+#' \describe{
+# ' RData large than >100 MB. If on github, then it won't have this .RData. Download here:
+# ' http://upnaesrv1.epfl.ch/JakeYeung_4CseqPaper_RData_Objects/fourcseq_objects_batch_3.RData
+#'   ...
+#' }
+"fourcseq_objects_batch_3"
